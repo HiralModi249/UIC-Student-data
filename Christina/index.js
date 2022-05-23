35b925a3c6080cbb38700db2b230825d62be8f80
421christina <106105696+421christina@users.noreply.github.com>
2022-05-23 12:38:29 -0500

    Add files via upload

---
+++ Christina/index.js
@@ -0,0 +1,478 @@
+// let i = "hello"
+// function myFunction (java)
+// {
+//     console.log(java)
+// }
+
+
+// myFunction(i)
+
+// function newFunction (java)
+// {
+//     let num = 2 * java
+//     // console.log(num)
+//     return num
+    
+   
+
+// }
+
+
+// console.log(newFunction(2))
+// newFunction(3)
+// newFunction(4)
+// newFunction(5)
+// newFunction(6)
+
+
+
+// let readCsvFile = "Majors.csv"
+// let csvFile = document.getElementById(readCsvFile)
+// function myFunction(csvFile){
+//     console.log(csvFile)
+// }
+
+
+let majors = 
+    ["Accounting",
+    "Acting",
+    "African American Studies",
+    "African-American Studies",
+    "AHS-Undeclared",
+    "Anthropology",
+    "Applied Psychology",
+    "Architectural Studies",
+    "Architecture",
+    "Art",
+    "Art Education",
+    "Art-Foundation",
+    "Art History",
+    "Biochemistry",
+    "Bioengineering",
+    "Biological Sciences",
+    "Biomedical Engineering",
+    "Black Studies",
+    "Bus Admin-Undeclared",
+    "Business Administration",
+    "Business Administration (MBA)",
+    "CADA-Undeclared",
+    "Campus Certificate Program",
+    "Chemical Engineering",
+    "Chemistry",
+    "Civil Engineering",
+    "Classical Civilization",
+    "Classical Languages & Lit",
+    "Classical Studies",
+    "Communication",
+    "Computer Engineering",
+    "Computer Science",
+    "Computer Sci & Linguistics",
+    "Criminal Justice",
+    "Criminology, Law, and Justice",
+    "CUPPA-Undeclared",
+    "Data Science",
+    "Dental Medicine",
+    "Dental Medicine (DMDAS Prgm)",
+    "Dental Medicine (IDDP)",
+    "Dentistry",
+    "Dentistry (IDP)",
+    "Design",
+    "Design Studies",
+    "Disability & Human Development",
+    "Earth & Environmental Sciences",
+    "Economics",
+    "Education - Undeclared",
+    "Electrical Engineering",
+    "Electronic Visualization",
+    "Elementary Education",
+    "Employee Benefits",
+    "Endodontics - Certificate",
+    "Engineering",
+    "Engineering Management",
+    "Engineering Nondegree-CE",
+    "Engineering Physics",
+    "Engineering - Pre-CS & Design",
+    "Engineering - Pre-Data Science",
+    "Engineering-Undeclared",
+    "English",
+    "Entrepreneurship",
+    "Estate Planning",
+    "Exercise Physiology",
+    "Finance",
+    "French",
+    "French and Francophone Studies",
+    "Gender and Women's Studies",
+    "Geography",
+    "Germanic Studies",
+    "Graphic Design",
+    "Graphic/Industrial Desgn-Found",
+    "Health Information Management",
+    "History",
+    "Human Development & Learning",
+    "Human Nutrition",
+    "Human Nutrition & Dietetics",
+    "Human Resource Management",
+    "Individual Plan of Study",
+    "Industrial Design",
+    "Industrial Engineering",
+    "Information & Decision Sci",
+    "Info Tech and Privacy Law",
+    "Integrated Design and Arts",
+    "Integrated Health Studies",
+    "Intellectual Property Law",
+    "Intensive English",
+    "InterdisciplinaryEducArts",
+    "Intl Business and Trade Law",
+    "Italian",
+    "Jazz Studies",
+    "Kinesiology",
+    "Kinesiology, Exercise Sci",
+    "Kinesiology - Undeclared",
+    "LAS Online Pathways Nondegree",
+    "LAS - PreHealth/PreScience",
+    "LAS Sciences Nondegree-CE",
+    "Latin Amer and Latino Studies",
+    "Latin American Studies",
+    "Law",
+    "Liberal Arts - Pre-CS & Ling",
+    "Liberal Arts - Pre-IHS",
+    "Liberal Arts - PreNeuroscience",
+    "Liberal Arts - Undeclared",
+    "Liberal Studies",
+    "Management",
+    "Management Science, General",
+    "Marketing",
+    "Math and Computer Science",
+    "Math & Computer Science",
+    "Mathematics",
+    "Mechanical Engineering",
+    "Medical Preparatory Nondegree",
+    "Medicine - Chicago",
+    "Movement Sciences",
+    "Movement Sciences-Undeclared",
+    "Moving Image",
+    "Music",
+    "Music Business",
+    "Neuroscience",
+    "Nondegree",
+    "Nondegree-CE",
+    "Nondegree Course-CE",
+    "Nursing (BSN)",
+    "Nursing Nondegree-CE",
+    "Nursing Practice",
+    "Nursing-R.N. Completion (BSN)",
+    "Nursing: RN to BSN Completion",
+    "Nutrition",
+    "Occupational Therapy (OTD)",
+    "Oral and Maxillof Surg - Cert",
+    "Orthodontics - Certificate",
+    "Pediatric Dentistry - Certific",
+    "PE: Pre-Teacher Ed, Kinsiol",
+    "Performance",
+    "Periodontics - Certificate",
+    "Pharmacy -  CCO (PharmD)",
+    "Pharmacy (PharmD)",
+    "Philosophy",
+    "Photo/Film/Electronic Media",
+    "Photography",
+    "Physical Ed, Community Phys Ed",
+    "Physical Ed,K-12 Certification",
+    "Physical Therapy",
+    "Physics",
+    "Polish",
+    "Polish, Russian & CEE Studies",
+    "Political Science",
+    "Pre-Art Education",
+    "Pre-Elementary Education",
+    "Pre-Health Information Mgmt",
+    "Pre-Human Nutrition",
+    "Pre-Medical Lab Science",
+    "Pre-Nursing",
+    "Pre-Occupational Therapy",
+    "Pre-Pharmacy",
+    "Pre-Physical Therapy",
+    "Pre-Social Work",
+    "Prosthodontics - Certificate",
+    "Psychology",
+    "Public Health",
+    "Public Health (DrPH)",
+    "Public Health (MPH)",
+    "Public Health - Undeclared",
+    "Public Policy",
+    "Real Estate",
+    "Real Estate Law",
+    "Rehabilitation Sciences",
+    "Russian",
+    "SCALES Program Nondegree",
+    "Social Work",
+    "Social Work Nondegree-CE",
+    "Sociology",
+    "Spanish",
+    "Spanish-Economics",
+    "Statistics",
+    "Stats & Operations Res",
+    "Studio Arts",
+    "Summer Session",
+    "Tax Law",
+    "Teaching of Biol Sciences",
+    "Teaching of Chemistry",
+    "Teaching of English",
+    "Teaching of French",
+    "Teaching of Geography",
+    "Teaching of German",
+    "Teaching of History",
+    "Teaching of Mathematics",
+    "Teaching of Physics",
+    "Teaching of Spanish",
+    "Theatre",
+    "Theatre and Performance",
+    "Theatre Design",
+    "TheatreDesign,Production&Tech",
+    "Urban and Public Affairs",
+    "Urban Education",
+    "Urban Studies"]
+   
+    
+    function chooseMajors (majors){
+        let length = majors.length
+        let Id =  Math.floor(Math.random() * length);
+        return majors[Id]
+    }
+    // console.log(chooseMajors(majors));
+    
+
+    let minors = [
+        "African-American Studies",
+        "Ancient Greek",
+        "Anthropology",
+        "Arabic",
+        "Architectural Studies",
+        "Art",
+        "Art History",
+        "Asian American Studies",
+        "Asian Studies",
+        "Bioengineering",
+        "Biological Sciences",
+        "Biology",
+        "Biomedical Engineering",
+        "Black Studies",
+        "Business Administration",
+        "Business Analytics",
+        "Business Operations",
+        "Business Statistics",
+        "Central & Eastern Eur Studies",
+        "Chemical Engineering",
+        "Chemistry",
+        "Chinese",
+        "Civic Analytics",
+        "Civil Engineering",
+        "Classical Civilization",
+        "Classical Studies",
+        "Communication",
+        "Computer Engineering",
+        "Computer Science",
+        "Criminal Justice",
+        "Criminology, Law, and Justice",
+        "Disability & Human Development",
+        "Earth & Environmental Sciences",
+        "Economics",
+        "Education",
+        "Electrical Engineering",
+        "English",
+        "English as a Second Language",
+        "Entrepreneurship",
+        "Environmental Engineering",
+        "Finance",
+        "Food Science",
+        "French",
+        "French and Francophone Studies",
+        "Gender and Women's Studies",
+        "Geographic Info. Systems",
+        "Geography",
+        "German",
+        "Germanic Studies",
+        "Global Asian Studies",
+        "History",
+        "Human Capital & Labor Markets",
+        "Human Resource Management",
+        "Industrial Engineering",
+        "Information Technology",
+        "Instructional Design/Training",
+        "International Business",
+        "International Studies",
+        "Italian",
+        "Italian & Italian Amer Studies",
+        "Jewish Studies",
+        "Kinesiology",
+        "Labor Markets &Workforce Devel",
+        "LAS International Studies",
+        "Latin",
+        "Latina/Latino Studies",
+        "Latin Amer and Latino Studies",
+        "Latin American Studies",
+        "Law and Society",
+        "Leadership",
+        "Life Science Visualization",
+        "Linguistics",
+        "Lithuanian Studies",
+        "Management",
+        "Management Information Systems",
+        "Managerial Skills",
+        "Materials Engineering",
+        "Math & Computer Science",
+        "Mathematics",
+        "Mechanical Engineering",
+        "Movement Sciences",
+        "Moving Image Arts",
+        "Museum and Exhibition Studies",
+        "Music",
+        "Native American Studies",
+        "Nonprofit Management",
+        "Nutrition",
+        "Philosophy",
+        "Physics",
+        "Polish",
+        "Polish Studies",
+        "Political Science",
+        "Professional Writing",
+        "Psychology",
+        "Public Health",
+        "Public Policy",
+        "Real Estate",
+        "Religious Studies",
+        "Russian",
+        "Russian Studies",
+        "Social Justice",
+        "Sociology",
+        "Spanish",
+        "Spanish for the Professions",
+        "Studio Arts",
+        "Supply Chain Management",
+        "Supply Chain & Operations Mgmt",
+        "Sustainable Cities",
+        "Teaching of English",
+        "Teaching of French",
+        "Teaching of German",
+        "Teaching of Physics",
+        "Teaching of Spanish",
+        "Theatre",
+        "Urban Planning & Policy",
+        "Urban Studies"]
+
+function chooseMinors (minors){
+    let length = minors.length
+    let Id =  Math.floor(Math.random() * length);
+    return minors[Id];
+}
+
+function generateUIN(){
+    let max = 100000000;
+    let min = 999999999;
+    let sum =  Math.floor(Math.random() * (max - min + 1)) + min;  
+    return sum      
+}
+
+function generateTerm(){
+    let mn = 22018;
+    let mx = 22022;
+    let temp = Math.floor(Math.random() * (mx - mn + 1)) + mn;
+    let array = [1,5,8];
+    let generateSem =  Math.floor(Math.random() * array.length);
+    let r1 = array[generateSem];
+    let sum = "" + temp  + r1 ;
+    return sum;  
+}
+let newgT = generateTerm();
+
+function Sem(p){
+    let num = [220181,220185,220188,220191,220195,220198,
+        220201,220205,220208,220211,220215,220218,
+        220221,220225,220228];
+    let tmp = [];
+    for (let x in num){
+        if (p == num[x]){ 
+            for (i = x; i < num.length; i++){
+                if (tmp){
+                    tmp.push(num[i]);
+                }
+            } 
+            return tmp;
+         } 
+    }
+}
+
+function length(){
+    let len = Sem(newgT);
+    return len.length;
+}
+
+function generateYear(y){
+    if (y < 3){
+        return "Freshman";
+    }
+    else if (y  > 2 && y < 5){
+      return "Sophomore";
+    }
+    else if (y > 4 && y < 7){
+       return "Junior";
+    }
+    else if (y > 6 && y < 16){
+        return "Senior";   
+    }
+}
+
+
+function Semester(){
+    let a = "termCode: " + Sem(newgT) ;
+    let b = "Major: " + chooseMajors(majors);
+    let c = "Minor: " + chooseMinors(minors);
+    let add = "" + a + " " +  b + " " +  c;
+
+    return add;
+}
+
+
+class student {
+    constructor(UIN,CurrentClassStanding, Semesters){
+        this.UIN = UIN;
+        this.CurrentClassStanding = CurrentClassStanding;
+        this.Semesters = Semesters;
+    } 
+}      
+
+
+let newInfo = new student(generateUIN(),generateYear(length()), Semester());
+console.log(newInfo);
+
+
+// ,Sem(newgT),chooseMajors(majors),chooseMinors(minors))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
