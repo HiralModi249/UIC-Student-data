63118e3f7a5b4929ded4c8bba5a72f006c495d48
Hiral Modi <68930714+HiralModi249@users.noreply.github.com>
2022-05-26 11:45:03 -0500

    Update extra.js

--- Hiral/extra.js
+++ Hiral/extra.js
@@ -49,3 +49,98 @@
 //     }
 //     return enrolled_terms;
 // };
+
+
+// class student  {
+//     constructor(UIN, semesters, cs) {
+//         this.UIN = UIN;
+//         this.semesters = semesters;
+//         this.cs = cs;
+//     }
+//     sem(Term, Major, Minor) {
+//         this.Term = Term;
+//         this.Major = Major;
+//         this,Minor = Minor;
+//     }
+
+// }
+
+// //create for loop
+// let data = [];
+// for (let i =0; i< 100; i++) {
+//     data[i]= new student(createUIN(), semesters(createTerm()), classStanding(length_array(semesters(createTerm()))) ); 
+// }
+// console.log(data);
+
+
+
+
+// function semesters(p1) {
+//     let list_of_sem = [220181, 220185, 220188, 220191, 220195, 220198, 
+//                 220201, 220205, 220208, 220211, 220215, 220218, 220221, 220225, 220228];
+    
+//     let enrolledTerms = [];
+//     const semObject = {};
+//     let len = list_of_sem.length;
+//     let major = programs(majors);
+//     let minor = programs(minors); 
+
+//     for (let x in list_of_sem) {     //checks for generated term in list
+//         if (p1 == list_of_sem[x]) {     //if exists then push everything in temp array for output
+//             for (let i = x; i < len; i++) {
+//                 if(enrolledTerms) {
+//                     enrolledTerms.push(list_of_sem[i]);  
+//                 }
+//             }       
+//         }   
+//     } 
+//     let enrolledSemesters = [] ;
+//     semObject.Term = enrolledTerms;
+//     semObject.Major = major;
+//     semObject.Minor = minor;
+//     for (let i = 0 ; i < enrolledTerms.length; i++) {
+//         enrolledSemesters[i] = "Termcode:"+semObject.Term[i] + "; " + "Major:"+ semObject.Major + "; "+ "Minor:" + semObject.Minor;
+//         // enrolledSemesters[i] = semObject.Term[i] + semObject.Major + semObject.Minor;
+//         enrolledSemesters.push(enrolledSemesters[i]);   //gives Object-> term major minor
+//     }
+    
+//     let randomIndex = Math.floor(Math.random()*enrolledTerms.length);
+//     //console.log("random index " + randomIndex);
+//     let selectTerm = enrolledTerms[randomIndex];
+//     let newMinor = random_ChangeProgram();
+//     console.log("new Minor" + newMinor);
+    
+//     for (let i = randomIndex; i<enrolledTerms.length; i++ ){
+//         //const forChange = enrolledSemesters.findIndex( (enrolledSem) => enrolledSem.minor = newMinor ) 
+//         //enrolledSemesters.splice(semObject.Minor, 1 , newMinor);
+//         if (random_ChangeProgram == 1) {
+//             newMinor = 
+//         }
+        
+//         if (semObject.Minor != newMinor) {
+//             semObject.Minor = newMinor;
+//             //enrolledSemesters[i] = "Termcode:"+semObject.Term[i] + "; " + "Major:"+ semObject.Major + "; "+ "newMinor:" + semObject.Minor;
+//             enrolledSemesters.push(enrolledSemesters.Minor);   //gives Object-> term major minor
+
+//         }
+//     }
+    
+//     return enrolledSemesters; 
+// }
+// console.log(semesters(220205));
+
+// function random_ChangeProgram () {
+//     let change = [0,1];
+//     let lengthOfChange = change.length; 
+//     //let lengthTemp = length_array(arr1);
+//     let randomChange = Math.floor(Math.random()*lengthOfChange);
+//     let doesChange = change[randomChange];
+//     let change_minor;
+//     if (doesChange == 1) {
+//         //change in minor
+//         change_minor = programs(minors);   //returns a minor
+//         return change_minor;
+//     }
+//     else return 0;
+//     // return change_minor;
+// }
